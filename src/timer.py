fe93d88341c0101a73f5e7eae046f94d0384dbd0
Bermmie-Laptop <cbchun@lsis.com>
2020-12-20 22:48:51 +0900

    readMe 파일 수정

--- src/timer.py
+++ src/timer.py
@@ -6,7 +6,7 @@ def isPeriod(df_page):
     today = datetime.today()
     today_30 = today - timedelta(days=31)
 
-    day_to_search = df_page["date"].min()
+    day_to_search = df_page["date"].min() # <--- "date"를 빼는게 좋을 것 같다
 
     condition = day_to_search > today_30
 
