0db0773e2495f321c93d440f61873ce0e41b1fb4
cbchun <bermmie1000@gmail.com>
2022-08-13 16:19:30 +0900

    1차 완성

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 import time
+import io
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
@@ -7,11 +11,21 @@ def main():
     driver = set_webdriver()
     get_web(driver)
     select_manufacturer(driver)
-    click_search(driver)
-    table = crwal_sold_info(driver)
-    move_next_page(driver)
-    # parse_text(table)
-    time.sleep(10)
+
+    appended = ""
+    for _ in range(25):
+        sold_info = crwal_sold_info(driver)
+        appended = append_sold_info(appended, sold_info)
+        move_next_page(driver)
+
+    sold_info = crwal_sold_info(driver)
+    appended = append_sold_info(appended, sold_info)
+
+    dataset = parse_text(appended)
+    dataset = preprocess(dataset)
+    plot_sales(dataset)
+
+    driver.close()
 
 
 def set_webdriver():
@@ -22,50 +36,110 @@ def set_webdriver():
 def get_web(driver):
     url = "http://www.encar.com/dc/dc_carsearchpop.do?method=soldoutCars&carType=for"
     driver.get(url)
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(10)
 
 
 def select_manufacturer(driver):
     selected_manufacturer = Select(driver.find_element_by_id("companySearch"))
     selected_manufacturer.select_by_value("013")  # 벤츠
+    __click_search(driver)
 
 
-def click_search(driver):
+def __click_search(driver):
     driver.find_element_by_class_name("sp.search").click()
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(10)
 
 
 def crwal_sold_info(driver):
-    table = driver.find_element_by_class_name("list.car_list").text
-    return table
+    sold_info = driver.find_element_by_class_name("list.car_list").text
+    return sold_info
+
+
+def append_sold_info(table, sold_info):
+    appended = "\n".join([table, sold_info])
+    return appended
 
 
 def move_next_page(driver):
     current_page = driver.find_element_by_class_name("current").text
-    # next_page =
-    # TODO: 이거 완성하기, 받아 오는 정보는 세달이 적당할 듯함
-    # TODO: 가격과 판매 신고 가격의 차이점이 뭔지 물어보기
+    current_page = int(current_page)
+    rescaled = current_page % 10
+    next_page = rescaled + 1
+
+    if rescaled == 0:
+        __next_page_list(driver)
+    else:
+        __next_page(driver, next_page)
 
 
-def parse_text(text):
-    lines = text.splitlines()
-    lines = lines[1:]
+def __next_page_list(driver):
+    driver.find_element_by_class_name("next").click()
+    driver.implicitly_wait(10)
 
-    for line in lines:
-        front = line.split(" ")[:-6]
-        # end = line.split("km")[1]
-        # print(front)
-        # print(end)
-    # while True:
-    #     line = text.readline()
 
-    #     if not line:
-    #         break
-    #     else:
-    #         front = line.split("km")[0]
-    #         end = line.split("km")[1]
-    #         print(front)
-    #         print(end)
+def __next_page(driver, next_page: int):
+    driver.find_element_by_xpath(
+        f'//*[@id="tabRival2"]/div/span[2]/a[{next_page}]'
+    ).click()
+    driver.implicitly_wait(10)
+
+
+def __convert_text(table):
+    liner = io.StringIO(table)
+    return liner
+
+
+def parse_text(table):
+    liner = __convert_text(table)
+
+    name = []
+    info = []
+
+    for line in liner.readlines():
+        if "차량정보" not in line:
+            line = line.strip()
+            vehicle_name = line.split(" ")[:-6]
+            vehicle_info = line.split(" ")[-6:]
+
+            name.append(vehicle_name)
+            info.append(vehicle_info)
+
+    dataset = pd.DataFrame(data={"name": name, "info": info})
+
+    return dataset
+
+
+def preprocess(dataset):
+    dataset["name"] = dataset["name"].apply(lambda x: " ".join(x[:]))
+    dataset["info"] = dataset["info"].apply(lambda x: " ".join(x[:]))
+    dataset.replace("", np.nan, inplace=True)
+    dataset.dropna(axis=0, inplace=True)
+    dataset.reset_index(drop=True, inplace=True)
+
+    dataset = pd.concat(
+        [dataset["name"], dataset["info"].str.split(" ", expand=True)], axis=1
+    )
+    dataset.columns = ["name", "year", "distance", "price", "_", "_", "date"]
+    dataset.drop(columns={"_"}, inplace=True)
+
+    # dataset["date"] = pd.to_datetime(dataset["date"], format="%Y/%m/%d")
+    dataset.sort_values(by=["date"], inplace=True)
+
+    dataset["distance"].replace(",", "", regex=True, inplace=True)
+    dataset["distance"].replace("km", "", regex=True, inplace=True)
+    dataset["distance"] = dataset["distance"].astype(int)
+
+    dataset["price"].replace(",", "", regex=True, inplace=True)
+    dataset["price"].replace("만원", "", regex=True, inplace=True)
+    dataset["price"] = dataset["price"].astype(int)
+
+    return dataset
+
+
+def plot_sales(dataset):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=dataset["date"], y=dataset["price"], mode="markers"))
+    fig.show()
 
 
 if __name__ == "__main__":
